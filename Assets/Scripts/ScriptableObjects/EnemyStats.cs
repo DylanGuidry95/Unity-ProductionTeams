974e230f16cae00b3550fd73f2bd31ee4a62c38a
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-06-02 18:10:13 -0500

    End of Day

---
+++ Assets/Scripts/ScriptableObjects/EnemyStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "", menuName = "EnemyStat")]
+public class EnemyStats : ScriptableObject
+{
+    public int ScoreValue;
+    public int Health;
+    private bool IsDead;
+    public bool IsInitialized;
+
+    public void Initialize()
+    {
+        IsInitialized = true;
+    }
+
+    public bool TakeDamage(int amount)
+    {
+        if(IsDead)
+            return IsDead;
+        Health -= amount;
+        if (Health <= 0)
+        {
+            Health = 0;
+            IsDead = true;            
+        }
+        return IsDead;
+    }        
+}
