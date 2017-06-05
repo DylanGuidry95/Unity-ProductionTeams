6e7c660ee2d7207f43e85a4d04d773c451d11029
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-06-05 10:18:51 -0500

    Enemy Score

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,7 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehaviour : MonoBehaviour
+public class EnemyBehaviour : MonoBehaviour, IDamageable, IDamager
 {
-    
+    [SerializeField]
+    private EnemyStats _EnemyStats;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GetType() == typeof(IDamageable))
+            other.GetComponent<IDamageable>().TakeDamage(_EnemyStats.AttackPower);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        _EnemyStats.TakeDamage(amount);
+    }
+
+    public void DoDamage(IDamageable target)
+    {
+        _EnemyStats.Attack(target);
+    }
 }
--- Assets/Scripts/ScriptableObjects/EnemyStats.cs
+++ Assets/Scripts/ScriptableObjects/EnemyStats.cs
@@ -1,30 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "", menuName = "EnemyStat")]
 public class EnemyStats : ScriptableObject
 {
     public int ScoreValue;
     public int Health;
+    public int AttackPower;
     private bool IsDead;
     public bool IsInitialized;
+    public EventEnemyDied OnEnemyDied;
 
     public void Initialize()
     {
         IsInitialized = true;
     }
 
-    public bool TakeDamage(int amount)
+    public void TakeDamage(int amount)
     {
-        if(IsDead)
-            return IsDead;
+        if (IsDead)
+            return;
         Health -= amount;
         if (Health <= 0)
         {
             Health = 0;
             IsDead = true;            
+            OnEnemyDied.Invoke(this);
         }
-        return IsDead;
-    }        
+    }
+
+    public void Attack(IDamageable target)
+    {
+        target.TakeDamage(AttackPower);
+    }
+
+    [SerializeField]
+    public class EventEnemyDied : UnityEvent<EnemyStats>
+    {
+        
+    }
 }
