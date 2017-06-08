6ec6de6b710e8cd3f9de9b290123754d64a45a50
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-06-08 17:58:07 -0500

    Refactor

---
+++ Assets/Scripts/AttackBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(TargetingBehaviour))]
+public class AttackBehaviour : MonoBehaviour, IDamager
+{
+    private TargetingBehaviour _TargetingBehaviour;
+    public Stat AttackDamage;
+    public Stat AttackDelay;
+
+    [SerializeField] private float AttackTimer;
+
+    public void DoDamage(IDamageable target)
+    {
+        target.TakeDamage(AttackDamage.Value);
+    }
+
+    protected virtual void Update()
+    {
+        if (AttackTimer >= AttackDelay.Value)
+            DoDamage(_TargetingBehaviour.Target.GetComponent<IDamageable>());
+        AttackTimer += Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/LarvaEnemyTargetBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class LarvaEnemyTargetBehaviour : TargetingBehaviour
+{
+    public NavMeshAgent Agent;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Agent = GetComponent<NavMeshAgent>();
+        OnTargetChanged.AddListener(RaycastToTarget);
+    }
+
+    private void RaycastToTarget(GameObject invokedGameObject)
+    {
+        if (Target == null || invokedGameObject != gameObject)
+            return;
+        RaycastHit objectHit;
+        var direction = (Target.transform.position - transform.position).normalized;
+        if (Physics.Raycast(transform.position, direction, out objectHit, Mathf.Infinity, TargetCollisionLayer))
+            if (objectHit.collider.gameObject == Target)
+            {
+                var hitPosition = objectHit.point;
+                hitPosition.y = 0;
+                Agent.destination = hitPosition;
+            }
+    }
+
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        if (Agent == null)
+            return;
+        Gizmos.color = Color.red;
+        var raise = new Vector3(0, 1, 0);
+        Gizmos.DrawLine(transform.position + raise, Target.transform.position + raise);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(Agent.destination + raise, 1);
+    }
+#endif
+}
